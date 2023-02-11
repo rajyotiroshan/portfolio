7c75bec9179b0af9169ea2d89e47e7642c9f1d27
rajan <rajyotiroshan@gmail.com>
2023-02-12 00:27:03 +0530

    project

--- src/components/right/Project.js
+++ src/components/right/Project.js
@@ -1,19 +1,21 @@
 const Project = () => {
   return (
     <article className="proj">
-      <img src="../../assets/projects-images/adoptme.jpg" alt="project img" />
       <h2 className="proj-hdr">Adopt Me</h2>
-      <p className="proj-desc">
-        A Platform where you can search for any pet available for adoption. You
-        can also post a request for pet adoption.
-      </p>
-      <h3>Technology Stack</h3>
-      <ul>
-        <li>React.js (Redux/React Router)</li>
-        <li>Node.js (Express.js)</li>
-        <li>AWS For deployment</li>
-        <li>AWS Dynamodb and AWS RDS</li>
-      </ul>
+      <div className="proj-img"></div>
+      <div className="proj-hover proj-hover-hide">
+        <p className="proj-desc">
+          A Platform where you can search for any pet available for adoption.
+          You can also post a request for pet adoption.
+        </p>
+        <h3>Technology Stack</h3>
+        <ul className="proj-tech-list">
+          <li>React.js (Redux/React Router)</li>
+          <li>Node.js (Express.js)</li>
+          <li>AWS For deployment</li>
+          <li>AWS Dynamodb and AWS RDS</li>
+        </ul>
+      </div>
     </article>
   );
 };
